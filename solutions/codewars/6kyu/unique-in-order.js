d1bd996a606b4ac73deef3904b99717672440769
Danil <danil.chevgan.00@gmail.com>
2023-09-05 00:00:23 +0600

    add 6kyu

---
+++ solutions/codewars/6kyu/unique-in-order.js
@@ -0,0 +1,8 @@
+// ❓DESCRIPTION:
+
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items
+// without any elements with the same value next to each other and preserving the original order of elements.
+
+// ✅ SOLUTIONS
+
+const uniqueInOrder = iterable => [...iterable].filter((el, i) => el !== iterable[i - 1]);
